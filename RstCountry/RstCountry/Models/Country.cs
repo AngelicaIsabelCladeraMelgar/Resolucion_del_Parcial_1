f985a4d55ac585155b54d3fc2219c257f0bf8ef7
AngelicaIsabelCladeraMelgar <a2019110253@estudiantes.upsa.edu.bo>
2020-09-30 20:21:57 -0400

    Completo sin publicar

---
+++ RstCountry/RstCountry/Models/Country.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RstCountry.Models
+{
+ 
+
+    public class Language
+    {
+        public string iso639_1 { get; set; }
+        public string iso639_2 { get; set; }
+        [Key]
+        public string name { get; set; }
+        public string nativeName { get; set; }
+    }
+
+    public class Country
+    {
+        [Key]
+        [Required]
+        [StringLength(3,MinimumLength =3)]
+        public string alpha3Code { get; set; }
+
+        [Required]
+        [StringLength(50,MinimumLength =3)]
+        public string name { get; set; }
+
+        [Required]
+        public List<string> callingCodes { get; set; }//cuando esta en corchetes es un arreglo
+        //siempre que haya corchetes poner asi el codigo
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string region { get; set; }
+
+        [Required]
+        [Range(1,999999999999999999)]
+        public double area { get; set; }
+
+        [Required]
+        public List<Language> languages { get; set; }
+
+        [Required]
+        [RegularExpression("^https://[A-Za-z0-9./]+$")]
+        public string flag { get; set; }
+
+    }
+
+
+}
